781b52ddae1bca6efde17ea61f8af46f84ca194e
Ryan Sherry <rsherrycomputers@gmail.com>
2018-07-12 16:49:36 -0700

    Added Butterknife and Timber dependencies

--- app/src/main/java/com/rsherry/bakingapp/MainActivity.java
+++ app/src/main/java/com/rsherry/bakingapp/MainActivity.java
@@ -3,11 +3,19 @@ package com.rsherry.bakingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //Logging with Timber
+
+        Timber.plant(new Timber.DebugTree());
     }
 }
